1581286094173a8ae76153e66777e56aadbf4d52
Alexis Girault <alexis.girault@kitware.com>
2015-12-04 11:05:16 -0500

    ENH: Add Save Matlab pathdef module

---
+++ Modules/MatlabModules/SavePath.m
@@ -0,0 +1,15 @@
+function outputParams=SavePath(inputParams)
+
+if strcmp(inputParams.savingDir,'/')
+    errordlg('Select a directory to save the files', 'I/O Error')
+    error('I/O Error: missing directory')
+end
+
+%% NIRFAST PATH
+NIRFASTPath=fullfile('..','NIRFast');
+addpath(genpath(NIRFASTPath));
+status = savepath(fullfile(inputParams.savingDir,'pathdef.m'));
+if status == 1
+  errordlg('Could not save pathdef.m', 'Writing Error')
+  error('Writing Error: Could not save pathdef.m)')
+end
