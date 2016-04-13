5802b72ba8751b4eced9123511ad7b4932738b27
Alexis Girault <alexis.girault@kitware.com>
2016-04-13 11:37:37 -0400

    BUG: Make sure startup.m file is created
    Not created sometimes on Windows,
    fix by Vince Rossi.

--- Modules/MatlabModules/AddPath.m
+++ Modules/MatlabModules/AddPath.m
@@ -7,22 +7,34 @@ NIRFASTPath=fullfile(libDir,'NIRFASTMatlab');
 
 %% MATLAB PATH
 pn = what('MATLAB');
+startupFilePath = pn.path;
 
 %% Open startup.m
-startupFilePath = fullfile(pn.path,'startup.m');
-startupDoesntExist = ~exist(startupFilePath, 'file');
-[fid msg] = fopen(startupFilePath,'a+');
+startupFileName = fullfile(startupFilePath,'startup.m');
+startupDidntExist = ~exist(startupFileName, 'file');
+fid = fopen(startupFileName,'a+t');
 
-%% Populate startup.m
-if startupDoesntExist
-  fprintf(fid,'%%% startup.m\n');
-  fprintf(fid,'%%  This script is executed everytime you start MatLab.\n\n');
+%% Make sure a startup file exists
+if fid == -1
+  disp('Generating new startup.m file since none was present.');
+  cd(startupFilePath)
+  fidm = fopen('startup.m','w');
+  fclose(fidm);
+  cd(curDir)
+  fid = fopen(startupFileName,'a+t');
 end
 
-fprintf(fid,'%%% Adding NIRFAST package to MatLab path\n');
-fprintf(fid,'NIRFASTMatlabPath = ''%s'';\n', NIRFASTPath);
+%% If startup file did not exist
+if startupDidntExist
+  fprintf(fid,'%% startup.m\n');
+  fprintf(fid,'%% This script is executed everytime you start MatLab.\n\n');
+end
+
+%% Populate startup file
+fprintf(fid,'%% Adding NIRFAST package to MatLab path\n');
+fprintf(fid,'NIRFASTPath = ''%s'';\n', NIRFASTPath);
 fprintf(fid,'disp(''Added NIRFAST-Matlab directory to the path:'');\n');
-fprintf(fid,'disp(NIRFASTMatlabPath);\n');
+fprintf(fid,'disp(NIRFASTPath);\n');
 fprintf(fid,'disp(''Edit startup.m to remove NIRFAST-Matlab from the path when starting MatLab.'');\n');
-fprintf(fid,'addpath(genpath(NIRFASTMatlabPath))\n\n');
+fprintf(fid,'addpath(genpath(NIRFASTPath))\n\n');
 fclose(fid);
