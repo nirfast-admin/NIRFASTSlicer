c015cb16345d30694d5a7b0690dc237e0321d615
Alexis Girault <alexis.girault@kitware.com>
2016-04-17 20:49:13 -0400

    ENH: link home doc to modules

--- Modules/Scripted/Home/Home.py
+++ Modules/Scripted/Home/Home.py
@@ -58,15 +58,15 @@ class HomeWidget:
 <br>
 The drop-down Modules are ordered to follow the basic workflow for processing DICOMS from conventional imaging modalities which are then used in optical image reconstruction.  As a quick reference, the basic steps involve:<br>
 <br>
-&nbsp; 1. Use the <b>DICOM</b> module to load your MRI/CT/US DICOMS<br><br>
-&nbsp; 2. Use <b>Volume Rendering</b> to view a 3D rendering of your DICOM images<br><br>
-&nbsp; 3. <b>Crop</b> the image volume to the tissue region of interest for which you will reconstruct optical images.<br><br>
-&nbsp; 4. Use the <b>Cast Scalar Volumes</b> module (cropped volume to "Short" format) to prepare to segment the tissue<br><br>
-&nbsp; 5. Use the <b>Editor (segment tissue)</b> module to segment the tissue into different tissue types which can guide the optical image reconstruction routines<br><br>
-&nbsp; 6. Use the <b>Markups</b> module to place optical source and detector positions.<br><br>
-&nbsp; 7. Use the <b>Create Mesh</b> module to generate a NIRFAST-compatible mesh from the segmented tissue (label maps)<br><br>
+&nbsp; 1. Use the <a href="#"><b>DICOM</b></a> module to load your MRI/CT/US DICOMS<br><br>
+&nbsp; 2. Use <a href="#"><b>Volume Rendering</b></a> to view a 3D rendering of your DICOM images<br><br>
+&nbsp; 3. <a href="#"><b>Crop</b></a> the image volume to the tissue region of interest for which you will reconstruct optical images.<br><br>
+&nbsp; 4. Use the <a href="#"><b>Cast Scalar Volumes</b></a> module (cropped volume to "Short" format) to prepare to segment the tissue<br><br>
+&nbsp; 5. Use the <a href="#"><b>Editor (segment tissue)</b></a> module to segment the tissue into different tissue types which can guide the optical image reconstruction routines<br><br>
+&nbsp; 6. Use the <a href="#"><b>Markups</b></a> module to place optical source and detector positions.<br><br>
+&nbsp; 7. Use the <a href="#"><b>Create Mesh</b></a> module to generate a NIRFAST-compatible mesh from the segmented tissue (label maps)<br><br>
 &nbsp; 8. After creating the mesh, follow tutorials on reconstructing optical images in the NIRFAST Matlab window<br><br>
-&nbsp; 9. Once images have been reconstructed use the <b>Import Optical Properties</b> module to view the optical parameter images overlaid on the original DICOMS<br>
+&nbsp; 9. Once images have been reconstructed use the <a href="#"><b>Import Optical Properties</b></a> module to view the optical parameter images overlaid on the original DICOMS<br>
 """
 
         # TEXTEDIT
@@ -74,7 +74,28 @@ The drop-down Modules are ordered to follow the basic workflow for processing DI
         self.HomeTextSection.setReadOnly(True)
         self.HomeTextSection.setText(text)
         self.HomeTextSection.setMinimumHeight(400)
+        self.HomeTextSection.connect('cursorPositionChanged()', self.slot)
         self.layout.addWidget(self.HomeTextSection)
 
         # SPACER
         self.layout.addStretch()
+
+    def slot(self):
+        pos = self.HomeTextSection.textCursor().position()
+
+        if pos >= 265 and pos <= 269 :
+            slicer.util.selectModule(slicer.moduleNames.DICOM)
+        elif pos >= 317 and pos <= 333 :
+            slicer.util.selectModule(slicer.moduleNames.VolumeRendering)
+        elif pos >= 384 and pos <= 388 :
+            slicer.util.selectModule(slicer.moduleNames.CropVolume)
+        elif pos >= 500 and pos <= 519 :
+            slicer.util.selectModule(slicer.moduleNames.CastScalarVolume)
+        elif pos >= 609 and pos <= 632 :
+            slicer.util.selectModule(slicer.moduleNames.Editor)
+        elif pos >= 762 and pos <= 769 :
+            slicer.util.selectModule(slicer.moduleNames.Markups)
+        elif pos >= 839 and pos <= 850 :
+            slicer.util.selectModule(slicer.moduleNames.Image2Mesh)
+        elif pos >= 1095 and pos <= 1120 :
+            slicer.util.selectModule(slicer.moduleNames.Mesh2Image)
